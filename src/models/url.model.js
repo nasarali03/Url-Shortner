d82c2bcec920f2fe5bad8d6f566d888172bae000
nasarali03 <nasarali1805@gmail.com>
2024-05-01 23:10:39 +0500

    User MVC

---
+++ src/models/url.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const urlScheema = new mongoose.Schema(
+  {
+    shortId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    redirectUrl: {
+      type: String,
+      required: true,
+    },
+    visitHistory: [{ timestamp: { type: Number } }],
+  },
+  { timestamps: true }
+);
+//
+
+const URL = mongoose.model("url", urlScheema);
+export default URL;
