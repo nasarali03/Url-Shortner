0bfc5d4ce176b0753647c252f1d19cec41f53099
nasarali03 <nasarali1805@gmail.com>
2024-05-01 23:11:48 +0500

    Handling static routes

--- src/routes/static.routes.js
+++ src/routes/static.routes.js
@@ -1,11 +1,15 @@
 import express from "express";
-import Url from "../models/url.js";
+import Url from "../models/url.model.js";
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   const allUrls = await Url.find({});
   res.render("index", { urls: allUrls });
+});
+
+router.get("/signup", (req, res) => {
+  res.render("Signup");
 });
 
 export default router;
