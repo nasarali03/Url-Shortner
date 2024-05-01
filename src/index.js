75e340e5719404988e37c40cdd8c36ff5f7da76b
nasarali03 <nasarali1805@gmail.com>
2024-05-01 23:08:19 +0500

    Add new routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import express from "express";
-import urlRouter from "./routes/url.js";
 import connectToMongo from "./connection.js";
 import path from "path";
-import URL from "./models/url.js";
+import URL from "./models/url.model.js";
+
 import dotenv from "dotenv";
+
+import urlRouter from "./routes/url.routes.js";
 import staticRouter from "./routes/static.routes.js";
+import userRouter from "./routes/user.routes.js";
 
 const app = express();
 
@@ -12,18 +15,21 @@ dotenv.config({
   path: "./.env",
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 console.log(PORT);
 app.use(express.json());
 connectToMongo(process.env.DB_URI).then(() => console.log("Mongodb Connected"));
 
-const view_path = path.resolve("views");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+const view_path = path.resolve("views");
 app.set("view engine", "ejs");
 app.set("views", view_path);
 
 app.use("/url", urlRouter);
 app.use("/", staticRouter);
+app.use("/user", userRouter);
 
 app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
