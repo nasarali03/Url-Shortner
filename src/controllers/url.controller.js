15ea3d0dc32bbd73813d3d637a2b87374a86376d
nasarali03 <nasarali1805@gmail.com>
2024-05-01 23:09:13 +0500

    Contoller for handling urls

---
+++ src/controllers/url.controller.js
@@ -0,0 +1,27 @@
+import { nanoid } from "nanoid";
+import URL from "../models/url.model.js ";
+
+async function handleGenerateShortUrl(req, res) {
+  const body = req.body;
+
+  if (!body.url) return res.status(400).json({ error: "Url is required" });
+  const shortID = nanoid(8);
+  await URL.create({
+    shortId: shortID,
+    redirectUrl: body.url,
+    visitHistory: [],
+  });
+
+  return res.render("index", { id: shortID });
+}
+
+async function handleGetAnalytics(req, res) {
+  const shortId = req.params.shortId;
+  const result = await URL.findOne({ shortId });
+  return res.json({
+    totalClicks: result.visitHistory.length,
+    analytics: result.visitHistory,
+  });
+}
+
+export { handleGenerateShortUrl, handleGetAnalytics };
