d7a6d791a1e66a8504ba54599cfbe202c810cc57
nasarali03 <nasarali1805@gmail.com>
2024-05-02 22:56:32 +0500

    Mongodb Connection

--- src/connection.js
+++ src/connection.js
@@ -4,7 +4,7 @@ async function connectToMongo(url) {
   try {
     return await mongoose.connect(url);
   } catch (error) {
-    console.log("Errorduring connection:", error);
+    console.log("Error during connection:", error);
   }
 }
 
