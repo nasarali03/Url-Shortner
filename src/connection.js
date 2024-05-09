e73a5bbfb24b5bf2af8d550de2e38b40eea82e34
nasarali03 <nasarali1805@gmail.com>
2024-05-09 23:11:38 +0500

    MongoDB Connection

--- src/connection.js
+++ src/connection.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 
-async function connectToMongo(url) {
+async function connectToMongo() {
   try {
-    return await mongoose.connect(url);
+    return await mongoose.connect(process.env.DB_URI);
   } catch (error) {
     console.log("Error during connection:", error);
+    process.exit(1);
   }
 }
 
